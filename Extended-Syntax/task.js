fe4393246b64420dbd787a163fb6dd20c6386763
SuvorkovRN <suvorkovRN@gmail.com>
2019-11-12 15:13:33 +0500

    Extended-Syntax changed logic in 2nd task & corrected mistake in 1st task

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -19,7 +19,7 @@ function getResult(a,b,c){
 
    if (D > 0) {
        x.push((-b + Math.sqrt(D)) / (2 * a));
-       x.push((-b + Math.sqrt(D)) / (2 * a));
+       x.push((-b - Math.sqrt(D)) / (2 * a));
    }
 
    if (D === 0) {
@@ -38,44 +38,20 @@ function calculateAverageRating(){
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-//     let sumMark;
-//     let averageMark;
-
-
-//     if (marks.length > 5) {
-//         console.log('Количество оценок больше 5, в расчет будут приняты только первые 5 оценок');
-//         marks = marks.slice(0, 5);
-//         for (let i = 0; i < marks.length; i++) {
-//             sumMark = marks[i] + sumMark;
-//         }
-//         averageMark = sumMark / 5;
-//     } else if (marks.length <= 5) {
-//         for (let i = 0; i < marks.length; i++) {
-//             sumMark = marks[i] + sumMark;
-//         }
-//          averageMark = sumMark / marks.length;
-//     }
-   
-//     return averageMark;
-// }
     
     let sumMark = 0;
     let averageMark = 0;
 
-
     if (marks.length > 5) {
         console.log('Количество оценок больше 5, в расчет будут приняты только первые 5 оценок');
         marks = marks.slice(0, 5);
-        for (let i = 0; i < marks.length; i++) {
-            sumMark = marks[i] + sumMark;
-        }
-        averageMark = sumMark / 5;
-    } else if (marks.length <= 5) {
-        for (let i = 0; i < marks.length; i++) {
-            sumMark = marks[i] + sumMark;
-        }
-         averageMark = sumMark / marks.length;
     }
+
+    for (let i = 0; i < marks.length; i++) {
+        sumMark = marks[i] + sumMark;
+    }
+    
+    averageMark = sumMark / marks.length;
    
     return averageMark;
 }
@@ -93,8 +69,9 @@ function askDrink(name,dateOfBirthday){
     let now = new Date();
     now = now.getFullYear();
     let result = '';
+    let age = now - yearOfBirthday;
 
-    if ((now - yearOfBirthday) < 18) {
+    if (age < 18) {
         result = 'Сожалею, ' + name + ', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!'
    } else {
         result = 'Не желаете ли олд-фэшн, ' + name + '?';
