247a603e22e33e13e1502a338568df13fbee6d20
SuvorkovRN <suvorkovRN@gmail.com>
2019-11-23 00:57:57 +0500

    Array-Proto-hocs ready for check

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,32 +1,41 @@
+function sum(...args) { 
+  let sum = 0;
+  console.log(sum);
+  console.log(args);
+  for (let arg of args) sum += arg;
+  console.log(sum);
+  return sum;
+}
 function compareArrays(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 console.log(compareArrays([8, 1, 2], [8, 1, 2]));
 
-const sum = (a, b) => a + b;
-
 function memoize(sum, limit) {
   let memory = [];
-
-  return function checksum( a, b ) {
-      
-    if (memory.some(argument => compareArrays(argument.arg, [a, b]))) {
-
-      let resultinMemory = memory.filter(argument => compareArrays(argument.arg, [a, b]));
-      resultinMemory = (resultinMemory[0].result);
-      console.log(`Ответ найден в памяти = ${resultinMemory}`);
-      return resultinMemory;
+  return function ( ...args ) {
+    console.log(args);
+    console.log(memory.some(argument => compareArrays(argument.arg, args)));
+    if (memory.some(argument => compareArrays(argument.arg, args))) {
+      console.log(memory[0].result);
+      console.log(`Ответ найден в памяти = ${memory[0].result}`);
+      return memory[0].result;
     }
 
-    memory.push({arg: [a, b], result: a + b});
+    let sumofArray = 0;
+    
+    for (let arg of args) sumofArray += arg;
+    memory.push({arg: args, result: sumofArray});
+    console.log(memory);
 
     if (memory.length > limit) {
       memory.splice(0, 1);
     }
     console.log(memory.length);
-    console.log(`В памяти ответ не найден, ответ расчитан функцией sum = ${sum(a, b)}`);
-    return sum( a, b );
+    console.log(memory);
+    console.log(`В памяти ответ не найден, ответ раcсчитан функцией sum = ${sum(...args)}`);
+    return sum( ...args );
   }
 
 }
@@ -39,78 +48,3 @@ console.log(mSum(2, 8));
 console.log(mSum(3, 7));
 console.log(mSum(4, 6));
 console.log(mSum(4, 6));
-
-// function sumAll(...args) { 
-//   let sum = 0;
-
-//   for (let arg of args) sum += arg;
-
-//   return sum;
-// }
-// console.log(sumAll(1, 1));
-// console.log(sumAll(1, 1, 1));
-// console.log(sumAll(1, 1, 1, 1));
-// console.log(sumAll(1, 1, 1, 1, 1));
-
-
-// function compareArrays(arr1, arr2) {
-//   return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
-// }
-
-// console.log(compareArrays([8, 1, 2], [8, 1, 2]));
-
-// function sum(...args) { 
-//   let sum = 0;
-
-//   for (let arg of args) sum += arg;
-
-//   return sum;
-// }
-
-// function memoize(sum, limit) {
-//   let memory = [];
-
-//   return function checksum( sum ) {
-//     console.log(sum);  
-//     if (memory.some(argument => compareArrays(argument.arg, args))) {
-
-//       let resultinMemory = memory.filter(argument => compareArrays(argument.arg, args));
-//       resultinMemory = (resultinMemory[0].result);
-//       console.log(`Ответ найден в памяти = ${memory}`);
-//       return resultinMemory;
-//     }
-//     let argSum = 0;
-//     for (let arg of args) argSum += arg;
-//     console.log(argSum);
-//     memory.push({arg: args, result: argSum});
-//     console.log(memory);
-//     console.log(limit);
-//     if (memory.length > limit) {
-//       memory.splice(0, 1);
-//     }
-//     console.log(`В памяти ответ не найден, ответ расчитан функцией sum = ${memory}`);
-//     return sum( args );
-//   }
-
-// }
-
-// const mSum = memoize(sum, 3);
-
-
-// console.log(mSum(1, 9));
-// console.log(mSum(2, 8));
-// console.log(mSum(3, 7));
-// console.log(mSum(4, 6));
-// console.log(mSum(4, 6));
-
-// function sumAll(...args) { 
-//   let sum = 0;
-
-//   for (let arg of args) sum += arg;
-
-//   return sum;
-// }
-// console.log(sumAll(1, 1));
-// console.log(sumAll(1, 1, 1));
-// console.log(sumAll(1, 1, 1, 1));
-// console.log(sumAll(1, 1, 1, 1, 1));
